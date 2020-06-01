ec500a501f185378143223b342d18a33020c8b5a
Prajwol Lamichhane <prajwollamichhane11@gmail.com>
2020-06-02 02:36:13 +0545

    src

---
+++ src/loadANDpredict.py
@@ -0,0 +1,41 @@
+# load_model_sample.py
+from keras.models import load_model
+from keras.preprocessing import image
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+def load_image(img_path, show=False):
+
+    img = image.load_img(img_path, target_size=(64, 64))
+    img_tensor = image.img_to_array(img)                    # (height, width, channels)
+    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+    img_tensor /= 255.                                      # imshow expects values in the range [0, 1]
+
+    if show:
+        plt.imshow(img_tensor[0])                           
+        plt.axis('off')
+        plt.show()
+
+    return img_tensor
+
+
+if __name__ == "__main__":
+
+    # load model
+    print("***")
+    model = load_model("model.h5")
+
+    # image path
+    img_path = './download.png'
+
+    print("********")
+
+    # load a single image
+    new_image = load_image(img_path)
+
+    # check prediction
+    pred = model.predict(new_image)
+
+    print(pred)
